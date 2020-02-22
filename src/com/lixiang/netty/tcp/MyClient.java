a0090c701c8507e981ac7ba0b7e278b4f5866865
lixiang <1368781197@qq.com>
2020-02-22 18:09:36 +0800

    tcp粘包、拆包引入

---
+++ src/com/lixiang/netty/tcp/MyClient.java
@@ -0,0 +1,34 @@
+package com.lixiang.netty.tcp;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Description //TODO
+ * @Author 李项
+ * @Date 2020/2/19
+ * @Version 1.0
+ */
+public class MyClient  {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new MyClientInitializer());  //自定义一个初始化类
+
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 7000).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            
+        }
+
+
+    }
+}
---
+++ src/com/lixiang/netty/tcp/MyClientInitializer.java
@@ -0,0 +1,21 @@
+package com.lixiang.netty.tcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @Description //TODO
+ * @Author 李项
+ * @Date 2020/2/22
+ * @Version 1.0
+ */
+public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new MyClientHandler());
+
+    }
+}
